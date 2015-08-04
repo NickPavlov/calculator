8d1f243b84b5910641ba48aca9412c34f323cc17
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-04 12:21:21 +0300

    model.parser.MathParser.parse() - check for empty input switched to ternary operator.

--- src/main/java/com/sysgears/calculator/model/parser/MathParser.java
+++ src/main/java/com/sysgears/calculator/model/parser/MathParser.java
@@ -69,11 +69,8 @@ public class MathParser implements IMathParser {
             throw new IllegalArgumentException("Expression can't be null.");
         }
         String result = MathConverter.removeEmptyBrackets(expression.replaceAll(" ", ""));
-        if (result.isEmpty()) {
-            result = "0";
-        }
 
-        return parseBrackets(result, result.length());
+        return result.isEmpty() ? "0" : parseBrackets(result, result.length());
     }
 
     /**
