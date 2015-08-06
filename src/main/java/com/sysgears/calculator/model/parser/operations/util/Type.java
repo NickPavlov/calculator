125f9838f2aa58cb9982f8de68f94aec4ea3f417
Nick Pavlov <pavlov.nikk@gmail.com>
2015-08-06 13:24:04 +0300

    parser.operations.utilType - regex removed.

--- src/main/java/com/sysgears/calculator/model/parser/operations/util/Type.java
+++ src/main/java/com/sysgears/calculator/model/parser/operations/util/Type.java
@@ -1,9 +1,5 @@
 package com.sysgears.calculator.model.parser.operations.util;
 
-import com.sysgears.calculator.model.parser.brackets.Brackets;
-
-import java.util.regex.Pattern;
-
 /**
  * The <code>Type</code> class provides a set of operation types.
  */
@@ -11,47 +7,16 @@ public enum Type {
 
     /**
      * The binary operation.
-     * If the operands are not in brackets, the mathematical signs are taken into account.
      */
-    BINARY {
-        public String getPattern(final String operator) {
-            return OPERAND + Pattern.quote(operator) + OPERAND;
-        }
-    },
+    BINARY,
 
     /**
      * The unary operation.
      */
-    UNARY {
-        public String getPattern(final String operator) {
-            return operator + OPERAND;
-        }
-    },
+    UNARY,
 
     /**
      * The constant.
      */
-    CONSTANT {
-        public String getPattern(final String operator) {
-            return operator;
-        }
-    };
-
-    /**
-     * The regular expression for the operand.
-     */
-    public static final String OPERAND = "(?<![\\d"+ Brackets.OPENING_BRACKETS + "])" + "[\\+-]?\\d+(\\.\\d+)?";
-
-    /**
-     * The operand pattern.
-     */
-    public static final Pattern OPERAND_PATTERN = Pattern.compile(Type.OPERAND);
-
-    /**
-     * Should return a regex pattern to getCommand a single operation.
-     *
-     * @param operator an operator
-     * @return operation regex pattern
-     */
-    public abstract String getPattern(final String operator);
+    CONSTANT
 }
